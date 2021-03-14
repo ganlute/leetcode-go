7cfb82bcc242729496ba1a32278b1c56fe91bb3c
glennlgan <glennlgan@tencent.com>
2021-03-14 22:36:20 +0800

    add code 1~4

---
+++ pkg/code_2/struct.go
@@ -0,0 +1,6 @@
+package code_2
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
---
+++ pkg/code_4/code.go
@@ -0,0 +1,42 @@
+package code_4
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	nums3 := make([]int, len(nums1)+len(nums2))
+	n := 0
+	i := 0
+	j := 0
+	for {
+		if i == len(nums1) {
+			break
+		}
+		if j == len(nums2) {
+			break
+		}
+		if nums1[i] < nums2[j] {
+			nums3[n] = nums1[i]
+			i++
+			n++
+		} else {
+			nums3[n] = nums2[j]
+			j++
+			n++
+		}
+
+	}
+	for i < len(nums1) {
+		nums3[n] = nums1[i]
+		i++
+		n++
+	}
+	for j < len(nums2) {
+		nums3[n] = nums2[j]
+		j++
+		n++
+	}
+	n = len(nums3)
+	if n%2 == 0 {
+		v := nums3[n/2] + nums3[n/2-1]
+		return float64(v) / float64(2)
+	}
+	return float64(nums3[n/2])
+}
