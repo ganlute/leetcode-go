90496984abe3c36ecb3fbcd02e8e577ef4d2ae8d
glennlgan <glennlgan@tencent.com>
2021-03-20 00:42:41 +0800

    code 1502

---
+++ pkg/leetcode/code_1502/code.go
@@ -0,0 +1,15 @@
+package code_1502
+
+import "sort"
+
+func canMakeArithmeticProgression(arr []int) bool {
+	sort.Ints(arr)
+	for i:=1;i < len(arr)-1;i++ {
+		if arr[i] - arr[i-1] == arr[i+1] - arr[i] {
+			continue
+		}else {
+			return false
+		}
+	}
+	return true
+}
