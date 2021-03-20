c17776ee541e515df9f71e8ebd16cc1f7fe67b3a
glennlgan <glennlgan@tencent.com>
2021-03-20 16:09:13 +0800

    code 121 update

--- pkg/leetcode/code_121/code.go
+++ pkg/leetcode/code_121/code.go
@@ -1,22 +1,25 @@
 package code_121
 
+// 前i天的利润 = max (前i-1天的利润, 第i天的价格 - 前i-1天最小价格)
+
 func maxProfit(prices []int) int {
-	maxValue := 0
-	i:=0
-	j := len(prices)-1
-	for i < j {
-		maxValue = max(maxValue, prices[j] - prices[i])
-		if prices[i] > prices[i+1] {
-			i = i +1
-			continue
-		}
-		if prices[j] < prices[j-1] {
-			j = j -1
-			continue
-		}
-		
+	minPrice := make([]int, 0)
+	maxPro := make([]int, 0)
+	// 初始化
+	minPrice = append(minPrice, prices[0])
+	maxPro = append(maxPro, 0)
+
+	for i := 1; i < len(prices); i++ {
+		minPrice = append(minPrice, min(minPrice[i-1], prices[i]))
+		maxPro = append(maxPro, max(maxPro[i-1], prices[i]-minPrice[i-1]))
 	}
-	return maxValue
+	return maxPro[len(prices)-1]
+}
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
 }
 func max(a int, b int) int {
 	if a > b {
