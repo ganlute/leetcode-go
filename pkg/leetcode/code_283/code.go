0efc89468db2b8f66b4afb49d0d77018b778c3fa
glennlgan <glennlgan@tencent.com>
2021-03-20 17:01:05 +0800

    update 283

--- pkg/leetcode/code_283/code.go
+++ pkg/leetcode/code_283/code.go
@@ -3,10 +3,15 @@ package code_283
 // 保持非零元素的相对顺序
 func moveZeroes(nums []int) {
 	for i := 0; i < len(nums); i++ {
+		found := false
 		for j := 0; j < len(nums)-1-i; j++ {
 			if nums[j] == 0 {
 				nums[j], nums[j+1] = nums[j+1], nums[j]
+				found = true
 			}
+		}
+		if !found {
+			break
 		}
 	}
 }
