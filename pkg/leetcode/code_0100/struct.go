cce492c1ae090691ca18c2889488f703db0112b2
glennlgan <glennlgan@tencent.com>
2021-03-27 16:04:27 +0800

    update

---
+++ pkg/leetcode/code_0100/struct.go
@@ -0,0 +1,7 @@
+package code_0100
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
---
+++ pkg/leetcode/code_0226/code.go
@@ -0,0 +1,17 @@
+package code_0226
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left, root.Right = invertTree(root.Right), invertTree(root.Left)
+	return root
+}
