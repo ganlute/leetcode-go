eb132a94798031f38ae35eeafc74ee9d83824c89
glennlgan <glennlgan@tencent.com>
2021-03-28 12:19:18 +0800

    这道题倒真是奇了怪了

--- pkg/leetcode/code_0029/code.go
+++ pkg/leetcode/code_0029/code.go
@@ -10,6 +10,14 @@ func divide(dividend int, divisor int) int {
 		sign = sign * -1
 		divisor = - divisor
 	}
+	if divisor == 1 {
+		result := sign * dividend
+		if result < -1<<31 || result > 1<<31-1 {
+			return 1<<31 - 1
+		} else {
+			return result
+		}
+	}
 	i := 0
 	for dividend >= divisor {
 		i++
